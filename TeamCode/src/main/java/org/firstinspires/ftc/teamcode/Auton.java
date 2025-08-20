8c55020843d008cdf78ff333936a0b76f787cd48
Yoshi <142450474+thisrandomexistantguy@users.noreply.github.com>
2025-08-20 16:31:16 -0400

    added slide shit to johnny8 teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
@@ -14,14 +14,14 @@ public class Auton {
 
     public void moveForwardInches(double inches,double speed){
         int tickTarget=(int) Math.round(inches*Y_INCH_TICKS);
-        johnny8.resetDriveEncoder();
+        johnny8.resetDriveEncoders();
         for(DcMotor x:johnny8.allDriveMotors){
             x.setTargetPosition(tickTarget);
             x.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
         johnny8.move(0,-speed,0);
         johnny8.waitForMotors();
-        johnny8.resetDriveEncoder();
+        johnny8.resetDriveEncoders();
     }
     public void moveBackwardInches(double inches, double speed){
         moveForwardInches(-inches,-speed);
@@ -29,7 +29,7 @@ public class Auton {
     public void moveRightInches(double inches,double speed){
         //tickTarget = position of motors
         int tickTarget=(int) Math.round(-inches*Y_INCH_TICKS);
-        johnny8.resetDriveEncoder();
+        johnny8.resetDriveEncoders();
         johnny8.motorFrontLeft.setTargetPosition(tickTarget);
         johnny8.motorFrontRight.setTargetPosition(-tickTarget);
         johnny8.motorBackLeft.setTargetPosition(-tickTarget);
@@ -57,7 +57,7 @@ public class Auton {
         johnny8.motorFrontRight.setTargetPosition(-tickTarget);
         johnny8.motorBackLeft.setTargetPosition(tickTarget);
         johnny8.motorBackRight.setTargetPosition(-tickTarget);
-        for(DcMotor x:Johnny8.allDriveMotors){
+        for(DcMotor x:johnny8.allDriveMotors){
             x.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         }
 
